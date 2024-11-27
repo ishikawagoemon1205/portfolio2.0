76ebf2bd2392227c1d272b68776acd90ad2caba4
atsuhiro <ishikawaatsuhito1205@gmail.com>
2024-11-27 16:29:34 +0900

    バージョン戻し

--- DB/works.ts
+++ DB/works.ts
@@ -75,7 +75,7 @@ export const DBWorkesDate: PortfolioProjectDB = {
         { type: "link", data: { text: "サイトリンク", url: 'https://main.d2ohg8yu2vb3z0.amplifyapp.com' }},
         { type: "link", data: { text: "GitHub リポジトリ", url: 'https://github.com/ishikawagoemon1205/portfolio2.0' }},
         { type: "sectionTitle", data: "今後記載予定内容" },
-        { type: "text", data: "作成難易度が高かった個所a" },
+        { type: "text", data: "作成難易度が高かった個所aaaaaaaaa" },
     ],
       links: [
         { title: "GitHub リポジトリ", url: "https://github.com/example/portfolio-website" },
